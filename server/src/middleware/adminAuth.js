19029563296109d031571cc9bc8f0ac1be7789c5
Gal Amar <106630289+Gal9amar@users.noreply.github.com>
2026-03-14 12:31:14 +0200

    remove: old middleware

--- server/src/middleware/adminAuth.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res, next) => {
-  const password = req.headers['x-admin-password'];
-  if (!password || password !== process.env.ADMIN_PASSWORD) {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
-  next();
-};
