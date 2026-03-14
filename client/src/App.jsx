8600981d8e7b003fc482fa9008099ef075eb3df4
Gal Amar <106630289+Gal9amar@users.noreply.github.com>
2026-03-14 12:38:53 +0200

    remove: history page and nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import DashboardPage from './pages/DashboardPage';
 import SchedulePage from './pages/SchedulePage';
-import HistoryPage from './pages/HistoryPage';
 import AdminPage from './pages/AdminPage';
 
 export default function App() {
@@ -13,7 +12,6 @@ export default function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<DashboardPage />} />
           <Route path="/schedule" element={<SchedulePage />} />
-          <Route path="/history" element={<HistoryPage />} />
           <Route path="/admin" element={<AdminPage />} />
         </Route>
       </Routes>
